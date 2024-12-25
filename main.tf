cb4a726a3ab2bde65b0cfebb735b07b934de030d
Miizzzukiii <139797055+Miizzzukiii@users.noreply.github.com>
2024-12-25 15:45:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,12 @@ resource "vcd_vapp_org_network" "PostgreSQL-routed-network" {
 # Создание диска для PostgreSQL VM (объем переделать)
 resource "vcd_vm_internal_disk" "postgresql-inf-d-01-disk1" {
   vapp_name     = vcd_vapp.vapp_InfraDev_postgresql.name
-  vm_name       = vcd_vapp_vm.postgresql-inf-d-01-disk1.name
+  vm_name       = vcd_vapp_vm.postgresql-inf-d-01.name
   size_in_mb    = "20480"  
   bus_type      = "parallel"
   storage_profile = var.storage_profile
   bus_number    = 1
   unit_number   = 1
+  depends_on = [vcd_vapp_vm.postgresql-inf-d-01]
 }
+
