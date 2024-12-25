d88399a0ff8d6fe4f5570bcd9fb3813c1052f92e
Miizzzukiii <139797055+Miizzzukiii@users.noreply.github.com>
2024-12-25 17:41:53 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,24 @@
-# Создание vApp
-resource "vcd_vapp" "vapp_InfraDev_test" {
-  name = "vapp-InfraDev-test"
+# Создание vApp для БД
+resource "vcd_vapp" "vapp_InfraDev_PostgreSQL" {
+  name = "vapp-InfraDev-PostgreSQL"
   power_on = "true"
 
   depends_on = [module.network.Net]
 }
 
-# Создание сети в vApp
-resource "vcd_vapp_org_network" "test-routed-network" {
-  vapp_name         = vcd_vapp.vapp_InfraDev_test.name
+# Создание сети в vApp для БД
+resource "vcd_vapp_org_network" "PostgreSQL-routed-network" {
+  vapp_name         = vcd_vapp.vapp_InfraDev_PostgreSQL.name
   org_network_name  = module.network.Net.name
 }
 
+# Создание диска для PostgreSQL VM (объем переделать)
+resource "vcd_vm_internal_disk" "postgresql-inf-d-01-disk1" {
+  vapp_name     = vcd_vapp.vapp_InfraDev_postgresql.name
+  vm_name       = vcd_vapp_vm.postgresql-inf-d-01-disk1.name
+  size_in_mb    = "20480"  
+  bus_type      = "parallel"
+  storage_profile = var.storage_profile
+  bus_number    = 1
+  unit_number   = 1
+}
