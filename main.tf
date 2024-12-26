d5fc1b6becc20db139c6acd10627ccb6807f69f2
Miizzzukiii <139797055+Miizzzukiii@users.noreply.github.com>
2024-12-26 12:42:15 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 # Установка необходимых зависимостей
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common git
 
-# Установка Docker - тут тогда как? учитывая ИБ
+# Установка Docker - тут тогда как? учитывая ИБ ПЕРЕДЕЛАТЬ ТУТ ВСЕ
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
