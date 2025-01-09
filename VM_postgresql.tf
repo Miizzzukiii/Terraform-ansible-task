8b2ea9639c7ca7fd21f2f301802eb57de9cda588
Miizzzukiii <139797055+Miizzzukiii@users.noreply.github.com>
2025-01-09 09:28:59 +0300

    Update VM_postgresql.tf

--- VM_postgresql.tf
+++ VM_postgresql.tf
@@ -61,7 +61,7 @@ sudo apt-get update && sudo apt-get upgrade -y
 # Установка необходимых зависимостей-ПЕРЕДЕЛАТЬ
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common git
 
-# Установка Docker - тут как тогда? exbnsdfz B,
+# Установка Docker - тут как тогда? 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
